e32e41ecbdc4e216d8ef4bc9ed9a13cd8f01f0cb
Pedro Dias <petermdias@gmail.com>
2012-09-13 14:35:30 +0100

    cleaning up

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -f /usr/bin/python ];
+then
+	echo "ERROR: python not installed!"
+	exit 1
+fi
+
 echo "Installing dependencies"
 wget http://peak.telecommunity.com/dist/ez_setup.py
 python ez_setup.py
@@ -11,10 +17,17 @@ pip install ConfigParser
 rm -rf ez_setup.py
 
 echo "Installing in /usr/local/capi"
-mkdir /usr/local/capi
+if [ -d "/usr/local/capi" ]; then
+	mkdir /usr/local/capi
+fi
 cp -rf ./capi* /usr/local/capi/
 
-echo "Installing init.d script /etc/init.d/capid"
-cp -f ./files/capid.conf /etc/capid.conf
+echo "Installing init.d script"
+if [ ! -f /etc/capid.conf ];
+then
+	cp ./files/capid.conf /etc/capid.conf
+fi
 cp -f ./files/capid /etc/init.d/capid
 chmod +x /etc/init.d/capid
+
+echo "Edit /etc/capid.conf to fit your needs and then start the service: service capid restart"
